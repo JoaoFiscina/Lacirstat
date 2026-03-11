782be40b281b9f41c5d1a93dd773f1b2ebbdfa89
JoaoFiscina <130847291+JoaoFiscina@users.noreply.github.com>
2026-03-11 17:11:03 -0300

    Update module.js

--- tests/t-student/module.js
+++ tests/t-student/module.js
@@ -5,15 +5,7 @@ export async function renderTestModule(ctx) {
     <div>
       <h3>${utils.escapeHtml(config.title)}</h3>
       <p>${utils.escapeHtml(config.description || "")}</p>
-
-      <h4>Como inserir os dados</h4>
-      <ul>
-        ${(config.inputGuide || [])
-          .map((item) => `<li>${utils.escapeHtml(item)}</li>`)
-          .join("")}
-      </ul>
-
-      <div class="loading">Módulo inicial carregado com sucesso.</div>
+      <div class="loading">Módulo carregado com sucesso.</div>
     </div>
   `;
 }
