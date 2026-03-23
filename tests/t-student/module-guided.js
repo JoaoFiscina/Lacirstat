b217792e5544e421812f1739af729d589a0a3210
JoaoFiscina <joao.victor.fiscina@gmail.com>
2026-03-23 17:06:51 -0300

    Create module-guided.js

---
+++ tests/t-student/module-guided.js
@@ -0,0 +1,38 @@
+import { createDatasusWizard } from '../../assets/js/datasus-wizard.js';
+import {
+  deriveIndependentTTest,
+  derivePairedTTest,
+  findBestNormalizedPair,
+  getCategoryOptions,
+  getMetricOptions,
+  getPrimaryMetricKey,
+  getTimeOptions
+} from '../../assets/js/datasus-normalizer.js';
+import {
+  parseDataset,
+  safeWelch,
+  safePaired,
+  renderAnalysisError,
+  buildResultMetricsHtml,
+  buildResultChartsHtml,
+  buildManualInterpretation
+} from './module.js';
+
+export async function renderTestModule(ctx) {
+  void createDatasusWizard;
+  void deriveIndependentTTest;
+  void derivePairedTTest;
+  void findBestNormalizedPair;
+  void getCategoryOptions;
+  void getMetricOptions;
+  void getPrimaryMetricKey;
+  void getTimeOptions;
+  void parseDataset;
+  void safeWelch;
+  void safePaired;
+  void renderAnalysisError;
+  void buildResultMetricsHtml;
+  void buildResultChartsHtml;
+  void buildManualInterpretation;
+  void ctx;
+}
